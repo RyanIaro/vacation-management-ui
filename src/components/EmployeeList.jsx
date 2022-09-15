2e7a767a82fbcf8435aabe8ade1d34df06435f1e
Ryan Iaro <98638811+RyanIaro@users.noreply.github.com>
2022-09-15 19:09:33 +0300

    Create EmployeeList.jsx

---
+++ src/components/EmployeeList.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+
+export function EmployeeList() {
+    return(
+        <>
+            <div className="list">
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Employee Id</td>
+                            <td>First name</td>
+                            <td>Last name</td>
+                            <td>Office</td>
+                            <td>Department</td>
+                            <td>Salary</td>
+                            <td>Starting date</td>
+                            <td>Authorized off-days</td>
+                        </tr>
+                        <tr>
+                            <td>Employee Id</td>
+                            <td>First name</td>
+                            <td>Last name</td>
+                            <td>Office</td>
+                            <td>Department</td>
+                            <td>Salary</td>
+                            <td>Starting date</td>
+                            <td>Authorized off-days</td>
+                        </tr>
+                        <tr>
+                            <td>Employee Id</td>
+                            <td>First name</td>
+                            <td>Last name</td>
+                            <td>Office</td>
+                            <td>Department</td>
+                            <td>Salary</td>
+                            <td>Starting date</td>
+                            <td>Authorized off-days</td>
+                        </tr>
+                        <tr>
+                            <td>Employee Id</td>
+                            <td>First name</td>
+                            <td>Last name</td>
+                            <td>Office</td>
+                            <td>Department</td>
+                            <td>Salary</td>
+                            <td>Starting date</td>
+                            <td>Authorized off-days</td>
+                        </tr>
+                        <tr>
+                            <td>Employee Id</td>
+                            <td>First name</td>
+                            <td>Last name</td>
+                            <td>Office</td>
+                            <td>Department</td>
+                            <td>Salary</td>
+                            <td>Starting date</td>
+                            <td>Authorized off-days</td>
+                        </tr>
+                        <tr>
+                            <td>Employee Id</td>
+                            <td>First name</td>
+                            <td>Last name</td>
+                            <td>Office</td>
+                            <td>Department</td>
+                            <td>Salary</td>
+                            <td>Starting date</td>
+                            <td>Authorized off-days</td>
+                        </tr>
+                        <tr>
+                            <td>Employee Id</td>
+                            <td>First name</td>
+                            <td>Last name</td>
+                            <td>Office</td>
+                            <td>Department</td>
+                            <td>Salary</td>
+                            <td>Starting date</td>
+                            <td>Authorized off-days</td>
+                        </tr>
+                        <tr>
+                            <td>Employee Id</td>
+                            <td>First name</td>
+                            <td>Last name</td>
+                            <td>Office</td>
+                            <td>Department</td>
+                            <td>Salary</td>
+                            <td>Starting date</td>
+                            <td>Authorized off-days</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
+}
